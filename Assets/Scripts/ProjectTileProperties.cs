4b7fae7c6b00197434d1a0e532b07cab830dcc7d
Anastasiya Sidletskaya <nastiasidletskaya@gmail.com>
2025-04-09 01:06:09 +0300

    Scripts & input system

---
+++ Assets/Scripts/ProjectTileProperties.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public struct ProjectTileProperties
+{
+    public Vector3 direction;
+    public Vector3 initialPosition;
+    public float initialSpeed;
+    public float mass;
+    public float drag;
+}
+
+
